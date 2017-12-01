6d8a3c8e2d2bfe11fb92fa4dcf37ddabe6ad97ed
david mark <markkariuki17@gmail.com>
2017-12-01 15:55:26 +0300

    add the indention space

--- user_test.py
+++ user_test.py
@@ -2,20 +2,20 @@ import unittest # Importing the unittest module
 from credentials import credential # Importing the contact class
 
 class TestCredentials(unittest.TestCase)
- '''
+    '''
     Test class that defines test cases for the contact class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
-    '''
-def setUp(self);
-    '''
-    set up method to run before each test casesself.
-    '''
-    self.new_user = User("davy","mark","markkariuki17@gmail.com") # create user object
+   '''
+    def setUp(self);
+'''
+set up method to run before each test casesself.
+'''
+self.new_user = User("davy","mark","markkariuki17@gmail.com") # create user object
 
 
-def test_init(self);
+    def test_init(self):
     '''
     test_init test case to test if the object is initialized properly
     '''
