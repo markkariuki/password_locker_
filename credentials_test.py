b6d0cd39b5bc4391d58ff56848228ce2ad26823f
david mark <markkariuki17@gmail.com>
2017-12-01 16:55:31 +0300

    correct the error in importing

---
+++ credentials_test.py
@@ -0,0 +1,4 @@
+import unittest
+from credentials import credential
+
+class credential(unittest.TestCase)
--- user_test.py
+++ user_test.py
@@ -1,27 +1,29 @@
 import unittest # Importing the unittest module
 from credentials import credential # Importing the contact class
 
-class TestCredentials(unittest.TestCase):
+class Credentials(unittest.TestCase):
     '''
     Test class that defines test cases for the contact class behaviours.
 
     Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-   '''
-    def setUp(self);
-'''
-set up method to run before each test casesself.
-'''
+         unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+
+        '''
+        set up method to run before each test casesself.
+        '''
+
 self.new_user = User("davy","mark","markkariuki17@gmail.com") # create user object
 
+def test_init(self):
 
-    def test_init(self):
     '''
     test_init test case to test if the object is initialized properly
     '''
-     self.assertEqual(self.new_contact.first_name,"davy")
-        self.assertEqual(self.new_contact.last_name,"mark")
-        self.assertEqual(self.new_contact.email,"markkariuki17@gmail.com")
+    self.assertEqual(self.new_user.first_name,"davy")
+    self.assertEqual(self.new_user.last_name,"mark")
+    self.assertEqual(self.new_user.email,"markkariuki17@gmail.com")
 
 
 if __name__ == '__main__':
