eb220a6b717aa05d24664ce22da94c2a4ccf9c10
david mark <markkariuki17@gmail.com>
2017-12-01 15:20:46 +0300

    add the import part to import the class and unittest

---
+++ user_test.py
@@ -0,0 +1,9 @@
+import unittest # Importing the unittest module
+from credentials import credential # Importing the contact class
+
+ '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
